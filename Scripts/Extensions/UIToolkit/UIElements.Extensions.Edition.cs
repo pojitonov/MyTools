60d29a2edfe77f272d0db4bd95cc8f2a369b628a
timurproko <timur.proko@gmail.com>
2025-11-23 19:18:35 +0100

    added ui toolkit

---
+++ Scripts/Extensions/UIToolkit/UIElements.Extensions.Edition.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.UIElements;
+
+public static partial class UIToolKitExtensions
+{
+    /// <summary>
+    /// Adds USS Styling to a VisualElement
+    /// <li>ShortCut for VisualElement.AddToClassList(...)</li>
+    /// <li>Supports multiple classes separated by spaces: USS("h-40 w-40")</li>
+    /// </summary>
+    /// <returns>the VisualElement</returns>
+    /// <seealso cref="VisualElement.AddToClassList"/>
+    public static T USS<T>(this T e, string A) where T : VisualElement
+    {
+        if (string.IsNullOrWhiteSpace(A)) return e;
+
+        var classes = A.Split(new[] { ' ' }, System.StringSplitOptions.RemoveEmptyEntries);
+        foreach (var className in classes)
+        {
+            e.AddToClassList(className);
+        }
+
+        return e;
+    }
+
+    /// <inheritdoc cref="USS{T}(T,string)"/>
+    public static T USS<T>(this T e, string A, string B) where T : VisualElement
+    {
+        e.AddToClassList(A);
+        e.AddToClassList(B);
+        return e;
+    }
+
+    /// <inheritdoc cref="USS{T}(T,string)"/>
+    public static T USS<T>(this T e, string A, string B, string C) where T : VisualElement
+    {
+        e.AddToClassList(A);
+        e.AddToClassList(B);
+        e.AddToClassList(C);
+        return e;
+    }
+
+    /// <inheritdoc cref="USS{T}(T,string)"/>
+    public static T USS<T>(this T e, string A, string B, string C, string D) where T : VisualElement
+    {
+        e.AddToClassList(A);
+        e.AddToClassList(B);
+        e.AddToClassList(C);
+        e.AddToClassList(D);
+        return e;
+    }
+
+    /// <summary>
+    /// Returns a list of elements having a specific USS class
+    /// </summary>
+    public static IEnumerable<VisualElement> WhereClassListContains(this IEnumerable<VisualElement> e, string A)
+    {
+        return e.Where(x => x.ClassListContains(A));
+    }
+
+    /// <summary>
+    /// Returns a list of elements not having a specific USS class
+    /// </summary>
+    public static IEnumerable<VisualElement> WhereClassListMisses(this IEnumerable<VisualElement> e, string A)
+    {
+        return e.Where(x => x.ClassListContains(A) == false);
+    }
+
+    /// <summary>
+    /// Returns a list of child elements having a specific USS class
+    /// </summary>
+    public static IEnumerable<VisualElement> ChildrenWithClass(this VisualElement e, string A)
+    {
+        return e.Children().Where(x => x.ClassListContains(A));
+    }
+
+    /// <summary>
+    /// Returns the first child element having a specific USS class
+    /// </summary>
+    public static VisualElement FirstChildWithClass(this VisualElement e, string A)
+    {
+        return e.Children().FirstOrDefault(x => x.ClassListContains(A));
+    }
+
+    /// <summary>
+    /// Returns the last child element having a specific USS class
+    /// </summary>
+    public static VisualElement LastChildWithClass(this VisualElement e, string A)
+    {
+        return e.Children().LastOrDefault(x => x.ClassListContains(A));
+    }
+
+    /// <summary>
+    /// Adds USS Styling to every element of a IEnumerable{VisualElement}
+    /// </summary>
+    /// <returns>the VisualElement</returns>
+    /// <seealso cref="VisualElement.AddToClassList"/>
+    public static IEnumerable<T> USS<T>(this IEnumerable<T> e, string A) where T : VisualElement
+    {
+        return e.ForEachAndReturn(x => x.USS(A));
+    }
+
+    /// <inheritdoc cref="USS{T}(IEnumerable{T},string)"/>
+    public static IEnumerable<T> USS<T>(this IEnumerable<T> e, string A, string B) where T : VisualElement
+    {
+        return e.ForEachAndReturn(x => x.USS(A, B));
+    }
+
+    /// <inheritdoc cref="USS{T}(IEnumerable{T},string)"/>
+    public static IEnumerable<T> USS<T>(this IEnumerable<T> e, string A, string B, string C) where T : VisualElement
+    {
+        return e.ForEachAndReturn(x => x.USS(A, B, C));
+    }
+
+    /// <inheritdoc cref="USS{T}(IEnumerable{T},string)"/>
+    public static IEnumerable<T> USS<T>(this IEnumerable<T> e, string A, string B, string C, string D)
+        where T : VisualElement
+    {
+        return e.ForEachAndReturn(x => x.USS(A, B, C, D));
+    }
+
+    private static IEnumerable<T> ForEachAndReturn<T>(this IEnumerable<T> source, Action<T> action)
+    {
+        foreach (var item in source)
+        {
+            action?.Invoke(item);
+        }
+
+        return source;
+    }
+
+    /// <summary>
+    /// Removes USS Styling to a VisualElement
+    /// <li>ShortCut for VisualElement.RemoveFromClassList(...)</li>
+    /// </summary>
+    /// <returns>the VisualElement</returns>
+    /// <seealso cref="VisualElement.RemoveFromClassList"/>
+    public static T RemoveUSS<T>(this T e, string A) where T : VisualElement
+    {
+        e.RemoveFromClassList(A);
+        return e;
+    }
+
+    /// <inheritdoc cref="RemoveUSS{T}(T,string)"/>
+    public static T RemoveUSS<T>(this T e, string A, string B) where T : VisualElement
+    {
+        e.RemoveFromClassList(A);
+        e.RemoveFromClassList(B);
+        return e;
+    }
+
+    /// <inheritdoc cref="RemoveUSS{T}(T,string)"/>
+    public static T RemoveUSS<T>(this T e, string A, string B, string C) where T : VisualElement
+    {
+        e.RemoveFromClassList(A);
+        e.RemoveFromClassList(B);
+        e.RemoveFromClassList(C);
+        return e;
+    }
+
+    /// <inheritdoc cref="RemoveUSS{T}(T,string)"/>
+    public static T RemoveUSS<T>(this T e, string A, string B, string C, string D) where T : VisualElement
+    {
+        e.RemoveFromClassList(A);
+        e.RemoveFromClassList(B);
+        e.RemoveFromClassList(C);
+        e.RemoveFromClassList(D);
+        return e;
+    }
+
+    public static T StateUSS<T>(this T e, bool state, string A) where T : VisualElement
+    {
+        if (state)
+            e.AddToClassList(A);
+        else
+            e.RemoveFromClassList(A);
+        return e;
+    }
+    
+    /// <summary>
+    /// Removes USS Styling to every element of a IEnumerable{VisualElement}
+    /// <li>about 8 times faster than the Linq version using the Query() method as a selector</li>
+    /// </summary>
+    /// <returns>the VisualElement</returns>
+    /// <seealso cref="VisualElement.RemoveFromClassList"/>
+    public static IEnumerable<T> RemoveUSS<T>(this IEnumerable<T> e, string A) where T : VisualElement
+    {
+        return e.ForEachAndReturn(x => x.RemoveFromClassList(A));
+    }
+
+    /// <summary>
+    /// Removes all USS Styling from a given VisualElement
+    /// </summary>
+    public static T ClearUSS<T>(this T e) where T : VisualElement
+    {
+        e.ClearClassList();
+        return e;
+    }
+
+    /// <summary>
+    /// Removes all USS Styling from a given VisualElement and then adds a new styling
+    /// </summary>
+    public static T OverrideUSS<T>(this T e, string A) where T : VisualElement
+    {
+        return e.ClearUSS().USS(A);
+    }
+
+    /// <summary>Sets the parent container of a Given VisualElement</summary>
+    /// <returns>The now parented child VisualElement</returns>
+    public static T Container<T, U>(this T element, U container) where T : VisualElement where U : VisualElement
+    {
+        container.Add(element);
+        return element;
+    }
+
+    /// <summary>
+    /// Inserts a VisualElement Before the given element in hierarchy
+    /// </summary>
+    public static void InsertBefore<T>(this VisualElement v, T v2) where T : VisualElement
+    {
+        if (v.parent == null)
+        {
+            throw new NullReferenceException(v.name + " has no parent");
+        }
+
+        v.parent.Insert(v.parent.IndexOf(v), v2);
+    }
+
+    /// <summary>
+    /// Inserts a VisualElement After the given element in hierarchy
+    /// </summary>
+    public static void InsertAfter<T>(this VisualElement v, T v2) where T : VisualElement
+    {
+        if (v.parent == null)
+        {
+            throw new NullReferenceException(v.name + " has no parent");
+        }
+
+        v.parent.Insert(v.parent.IndexOf(v) + 1, v2);
+    }
+
+    /// <summary>Adds a list of VisualElements as children elements to a given VisualElement</summary>
+    public static void AddMany<T>(this VisualElement v, T elements) where T : IEnumerable<VisualElement>
+    {
+        foreach (var e in elements)
+        {
+            v.Add(e);
+        }
+    }
+}
---
+++ Scripts/Extensions/UIToolkit/UIElements.Extensions.Update.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public static partial class UIToolKitExtensions
+{
+    public static void TranslateToMousePositionClamped(this VisualElement e, Vector2 mousePosition)
+    {
+        // e.Translate(math.min(mousePosition, math.max(e.panel.visualTree.worldBound.size - e.worldBound.size, 0)));
+        var translation = math.clamp(mousePosition, 0, e.panel.visualTree.worldBound.size - e.worldBound.size);
+        e.Left(translation.x);
+        e.Top(translation.y);
+    }
+
+    public static void RemoveOnClickOutside(this VisualElement e)
+    {
+        e.RegisterOnClickOutside(e.RemoveFromHierarchy);
+    }
+
+    public static void HideOnClickOutside(this VisualElement e)
+    {
+        e.RegisterOnClickOutside(() => e.Hide());
+    }
+
+    public static void RegisterOnClickOutside(this VisualElement e, Action action)
+    {
+        e.panel.visualTree.Callback(OnClickOutsideWindow, TrickleDown.TrickleDown);
+
+        void OnClickOutsideWindow(MouseDownEvent evt)
+        {
+            if (evt.button == 0 && !e.layout.Contains(evt.mousePosition))
+            {
+                action?.Invoke();
+            }
+        }
+    }
+
+    public static void Callback(this VisualElement e, EventCallback<KeyDownEvent> action,
+        TrickleDown trickleDown = TrickleDown.TrickleDown) => e.RegisterCallback(action, trickleDown);
+
+    public static void Callback(this VisualElement e, EventCallback<KeyUpEvent> action,
+        TrickleDown trickleDown = TrickleDown.TrickleDown) => e.RegisterCallback(action, trickleDown);
+
+    public static void Callback(this VisualElement e, EventCallback<GeometryChangedEvent> action,
+        TrickleDown trickleDown = TrickleDown.TrickleDown) => e.RegisterCallback(action, trickleDown);
+
+    public static void Callback(this VisualElement e, EventCallback<MouseDownEvent> action,
+        TrickleDown trickleDown = TrickleDown.TrickleDown) => e.RegisterCallback(action, trickleDown);
+
+    public static void Callback(this VisualElement e, EventCallback<MouseUpEvent> action,
+        TrickleDown trickleDown = TrickleDown.TrickleDown) => e.RegisterCallback(action, trickleDown);
+
+    public static void Callback(this VisualElement e, EventCallback<MouseMoveEvent> action,
+        TrickleDown trickleDown = TrickleDown.TrickleDown) => e.RegisterCallback(action, trickleDown);
+
+    public static void Callback(this VisualElement e, EventCallback<MouseEnterEvent> action,
+        TrickleDown trickleDown = TrickleDown.TrickleDown) => e.RegisterCallback(action, trickleDown);
+
+    public static void Callback(this VisualElement e, EventCallback<MouseLeaveEvent> action,
+        TrickleDown trickleDown = TrickleDown.TrickleDown) => e.RegisterCallback(action, trickleDown);
+
+    public static void Callback(this VisualElement e, EventCallback<WheelEvent> action,
+        TrickleDown trickleDown = TrickleDown.TrickleDown) => e.RegisterCallback(action, trickleDown);
+
+    public static void Callback(this VisualElement e, EventCallback<FocusOutEvent> action,
+        TrickleDown trickleDown = TrickleDown.TrickleDown) => e.RegisterCallback(action, trickleDown);
+
+    public static void Callback(this VisualElement e, EventCallback<FocusInEvent> action,
+        TrickleDown trickleDown = TrickleDown.TrickleDown) => e.RegisterCallback(action, trickleDown);
+
+    public static void Callback(this VisualElement e, EventCallback<BlurEvent> action,
+        TrickleDown trickleDown = TrickleDown.TrickleDown) => e.RegisterCallback(action, trickleDown);
+
+    public static void Callback(this VisualElement e, EventCallback<FocusEvent> action,
+        TrickleDown trickleDown = TrickleDown.TrickleDown) => e.RegisterCallback(action, trickleDown);
+
+    public static void Callback(this VisualElement e, EventCallback<AttachToPanelEvent> action,
+        TrickleDown trickleDown = TrickleDown.TrickleDown) => e.RegisterCallback(action, trickleDown);
+
+    public static void Callback(this VisualElement e, EventCallback<DetachFromPanelEvent> action,
+        TrickleDown trickleDown = TrickleDown.TrickleDown) => e.RegisterCallback(action, trickleDown);
+
+    public static void Callback(this VisualElement e, EventCallback<CustomStyleResolvedEvent> action,
+        TrickleDown trickleDown = TrickleDown.TrickleDown) => e.RegisterCallback(action, trickleDown);
+
+    public static void Callback(this VisualElement e, EventCallback<ExecuteCommandEvent> action,
+        TrickleDown trickleDown = TrickleDown.TrickleDown) => e.RegisterCallback(action, trickleDown);
+
+    public static void Callback(this VisualElement e, EventCallback<ValidateCommandEvent> action,
+        TrickleDown trickleDown = TrickleDown.TrickleDown) => e.RegisterCallback(action, trickleDown);
+
+    public static void Callback(this VisualElement e, EventCallback<InputEvent> action,
+        TrickleDown trickleDown = TrickleDown.TrickleDown) => e.RegisterCallback(action, trickleDown);
+
+    public static void Callback(this VisualElement e, EventCallback<ClickEvent> action,
+        TrickleDown trickleDown = TrickleDown.TrickleDown) => e.RegisterCallback(action, trickleDown);
+
+    public static void Callback<T>(this VisualElement e, EventCallback<ChangeEvent<T>> action,
+        TrickleDown trickleDown = TrickleDown.TrickleDown) => e.RegisterCallback(action, trickleDown);
+
+    public static void Callback<T>(this VisualElement e, EventCallback<T> action,
+        TrickleDown trickleDown = TrickleDown.TrickleDown) where T : EventBase<T>, new()
+        => e.RegisterCallback(action, trickleDown);
+
+    // todo find why these can't be included in build
+    // public static void Callback(this VisualElement e, EventCallback<DragUpdatedEvent> action, TrickleDown trickleDown = TrickleDown.TrickleDown) => e.RegisterCallback(action, trickleDown);
+    // public static void Callback(this VisualElement e, EventCallback<DragPerformEvent> action, TrickleDown trickleDown = TrickleDown.TrickleDown) => e.RegisterCallback(action, trickleDown);
+    // public static void Callback(this VisualElement e, EventCallback<DragExitedEvent> action, TrickleDown trickleDown = TrickleDown.TrickleDown) => e.RegisterCallback(action, trickleDown);
+    // public static void Callback(this VisualElement e, EventCallback<DragEnterEvent> action, TrickleDown trickleDown = TrickleDown.TrickleDown) => e.RegisterCallback(action, trickleDown);
+
+    public static void RegisterOnMouseLeave(this VisualElement e, Action action)
+    {
+        e.RegisterCallback<MouseLeaveEvent>(_ => action?.Invoke());
+    }
+
+    public static void HideOnMouseLeave(this VisualElement e) => e.RegisterOnMouseLeave(() => e.Hide());
+    public static void RemoveOnMouseLeave(this VisualElement e) => e.RegisterOnMouseLeave(e.RemoveFromHierarchy);
+
+    public static void OnEscapeKeyPressed(this VisualElement e, Action action)
+    {
+        e.RegisterCallback<KeyDownEvent>(evt =>
+        {
+            if (evt.keyCode == KeyCode.Escape)
+            {
+                action.Invoke();
+            }
+        });
+    }
+
+    public static void DisplayTargetOnHover(this VisualElement e, VisualElement target)
+    {
+        e.RegisterCallback<MouseEnterEvent>(_ => target.Show());
+        e.RegisterCallback<MouseLeaveEvent>(_ => target.Hide());
+    }
+
+    /// <summary>
+    /// The default state of the target is invisible
+    /// </summary>
+    public static void MakeTargetVisibleOnHover(this VisualElement e, VisualElement target)
+    {
+        target.visible = false;
+        e.RegisterCallback<MouseEnterEvent>(_ => target.Visible(true));
+        e.RegisterCallback<MouseLeaveEvent>(_ => target.Visible(false));
+    }
+}
